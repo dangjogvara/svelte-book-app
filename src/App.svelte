f3ec2e5a5566a894bc6c328b9f38685384dbc12d
Dan Poulsen <dangjogvara@gmail.com>
2022-09-13 11:30:44 +0100

    setup start

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,5 @@
-<script lang="ts">
-  import Counter from './lib/Counter.svelte';
-</script>
-
-<main class="flex flex-col m-3">
-  <h1>Vite + Svelte</h1>
-
-  <div>
-    <Counter />
-  </div>
+<main class="flex flex-col m-3 text-center">
+  <h1>My Book Library</h1>
 </main>
 
 <style>
--- src/lib/Counter.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  let count: number = 0;
-  const increment = () => {
-    count += 1;
-  };
-</script>
-
-<button class="bg-red-800 rounded-sm text-center" on:click={increment}>Increment</button>
